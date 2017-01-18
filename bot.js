b5ab2089c89ca1a9ce38fbf2b90763a42e42135e
jjplay175 <jjplay175@hotmail.com>
2017-01-18 23:52:48 +0000

    V 1.0.4 Glossary added, Info expanded.
    Added a glossary command.
    Added all terms to info.

--- bot.js
+++ bot.js
@@ -50,11 +50,6 @@ UnityBot.on("message", function (message) {
     msg = message.content.split(" ").slice(1).join(" ").trim();
     msgl = message.content.toLowerCase().split(" ").slice(1).join(" ").trim();
 
-
-    if (cmd == "update" && message.author.id == "130329656476827648") {
-        message.delete().then(message.channel.sendMessage("**Updating**"))
-        BotActive = false;
-    }
     if (cmd == "activate" && message.author.id == "130329656476827648") {
         message.channel.sendMessage("**Functions Enabled**")
         BotActive = true;
@@ -75,6 +70,11 @@ UnityBot.on("message", function (message) {
     if (!message.content.startsWith("!")) return;
 
     if (BotActive === false) return message.reply("Functions are currently offline (Update in progress?)");
+
+    if (cmd == "update" && message.author.id == "130329656476827648") {
+        message.delete().then(message.channel.sendMessage("**Updating**"))
+        BotActive = false;
+    }
 
     if (cmd == "roleid" && message.author.id == AuthFile.botinfo.owner) {
         if (msg === undefined) {
@@ -187,8 +187,127 @@ UnityBot.on("message", function (message) {
             message.channel.sendMessage("__**Orochi Guide**__\n<http://forums.ubi.com/showthread.php/1509277>")
         }
     }
+    if (cmd == "glossary"){
+        message.reply("Thanks to HandheldBrando for this data!\n(Art of Battle, Bleed, Block, Chain/Combo, Chain/Combo finisher, Counter Guardbreak, Dash/Dodge, Dash/Dodge attacks, Deflect, Displacement, Exhausted/Exhaustion, Feints/Cancels, Guard Stance, Guardbreak, Guardbreak throw, Heavy attack, Light attack, Perfect Defence, Unblockable, Uninterruptible status, Hyper Armour, Super Armour, Dazed, Wall Stun, Superior Block, Chip damage/block damage, Command Grab, Poke, Safe, Punish, Priority, Reading, Recovery, Neutral, Neutral stance, Whiff, Zoning, Tracking, Weapon effects)")
+    }
     if (cmd == "info") {
-        console.log("Info ran")
+        if (msgl == "art of battle"){
+            message.channel.sendMessage("**Art of Battle:**\nThe combat system unique to For Honor. It incorporates 3 stances to both attack and defends on.");
+        }
+        if (msgl == "bleed"){
+            message.channel.sendMessage("**Bleed:**\nDamage over time effect.");
+        }
+        if (msgl == "block"){
+            message.channel.sendMessage("**Block:**\nTo prevent your enemy from successfully hitting you.\nBlocking a light attack will stop a chain/combo (Aka Superior Block)\nBlocking a heavy attack will not stop the enemy from chain/combo attacks (unless the hero possesses Superior Block) and will also result in a very small amount of block damage/chip damage.");
+        }
+        if (msgl == "Chain/Combo"){
+            message.channel.sendMessage("**Chain/Combo:**\nAttacks that link directly into other attacks and allow you to perform unique attacks or abilities when done in a specific order.");
+        }
+        if (msgl == "Chain/Combo finisher"){
+            message.channel.sendMessage("**Chain/Combo finisher:**\nAttacks that usually end a chain/combo. Some combo/chain finishers will do extra damage.");
+        }
+        if (msgl == "Counter Guardbreak"){
+            message.channel.sendMessage("**Counter Guardbreak:**\nUsing your own guard break to prevent the enemy from guard breaking you. This is done by pressing the guard breaks within a specific window during the enemies guard break animation.");
+        }
+        if (msgl == "dash" || msgl == "dodge" || msgl == "dash/dodge"){
+            message.channel.sendMessage("**Dash/Dodge:**\nTo evade an attack or to lunge in a direction.\nUsually done to avoid an attack or to close the gap between you and your opponent.");
+        }
+        if (msgl == "dash attack" || msgl == "dodge attack" || msgl == "dash/dodge attack"){
+            message.channel.sendMessage("**Dash/Dodge attacks:**\nAttacks that are initiated when dashing/dodging (I.E Orochi dashing side attacks or Kensei dashing forward light attack)");
+        }
+        if (msgl == "deflect"){
+            message.channel.sendMessage("**Deflect:**\nDeflect is a unique mechanic given to the Assassin heroes which allows them to prevent damage from an enemy attack and retaliate in some way. Each deflect follow up is unique to the character performing it.\nThis is done by dashing/dodging into an attack at the same time you would parry an attack. You do not need your guard in the same direction as an attack to successfully deflect.");
+        }
+        if (msgl == "displacement"){
+            message.channel.sendMessage("**Displacement:**\nAn attack or ability which repositions the enemy character or creates space between you and your opponent.");
+        }
+        if (msgl == "guard stance"){
+            message.channel.sendMessage("**Guard Stance:**\nEvery character (except assassins) have a static guard stance which is done by locking onto the opponent. In the guard stance, you have 3 different stances (top stance, left stance and right stance). You use these 3 stances to determine which side you will block and attack on.\n\nExample, If an opponent attacks your left side then you will want to be in the left stance to block them. If your opponents stance is in the top stance then you want to attack him on the left or right stance.\nAssassins are different, they have a Neutral stance because they do not have a static block and can only block for short durations. After that duration, they go back to a neutral stance.");
+        }
+        if (msgl == "exhausted" || msgl == "exhaustion" || msgl == "exhausted/exhaustion"){
+            message.channel.sendMessage("**Exhausted/Exhaustion:**\nWhen you run out of stamina.");
+        }
+        if (msgl == "feints" || msgl == "cancels" || msgl == "feints/cancels" || msgl == "feint" || msgl == "cancel"){
+            message.channel.sendMessage("**Feints/Cancels:**\nFeints: Feints are done by pressing the Feint button and this is characterized by outright canceling the attack and going back into a neutral stance.\nCancels: This is where things get tricky. A cancel is a feint except it does not require you to press the feint button and instead, it will always lead to a specific action afterward (I.E Raider heavy attack then pressing the light attack button to cancel into Pommel strike). ");
+        }
+        if (msgl == "guardbreak"){
+            message.channel.sendMessage("**GuardBreak:**\nThe act of grabbing your opponent and temporarily disabling his defences.");
+        }
+        if (msgl == "guardbreak throw"){
+            message.channel.sendMessage("**GuardBreak throw:**\nGuardBreak throws are initiated by successfully landing a GuardBreak and then pressing the GuardBreak button again while pressing one of the movement directions to throw your opponent in that direction (this can lead to Wall stuns/Environmental kills).");
+        }
+        if (msgl == "heavy attack"){
+            message.channel.sendMessage("**Heavy attack:**\nHard hitting but slow attacks. If blocked staggers the opponent (unless they possess Superior Block) and allows you to continue your chain");
+        }
+        if (msgl == "light attack"){
+            message.channel.sendMessage("**Light attack:**\nAttacks that are meant to be quick and do small amounts of damage but if it is blocked then you are staggered briefly and can no longer continue a chain.");
+        }
+        if (msgl == "perfect defence" || msgl == "perfect defense"){
+            message.channel.sendMessage("**Perfect Defence:** Omni-directional block (blocking all sides simultaneously) that constantly drains Stamina while preventing the user from counter guard breaking. Unique to Conqueror and Warlord.");
+        }
+        if (msgl == "unblockable"){
+            message.channel.sendMessage("**Unblockable:**\nVery powerful attacks that are usually very slow.They cannot be blocked through normal means and must be parried or dodged. Some abilities also have the unblockable status (I.E Raider sprinting grab/Warden Shoulder charge) and these abilities have to be dodged as there is no way to block abilities.");
+        }
+        if (msgl == "Uninterruptible status" || msgl == "uninterruptible"){
+            message.channel.sendMessage("**Uninterruptible status:**\nThis has 2 subtypes. Hyper armor and Super armor. Uninterruptible does not mean you do not take damage it only means your animation cannot be interrupted by attacks.");
+        }
+        if (msgl == "hyper armour" || msgl == "hyper armor"){
+            message.channel.sendMessage("**Hyper Armor:**\nCan withstand multiple hits without being interrupted (I.E Kensei would have hyper armor on a side heavy chain finisher). There are exceptions to this rule as some specific attacks and abilities will forcibly bypass uninterruptible status(I.E Orochi Storm Rush/Raider sprinting grab/Warden shoulder Charge)");
+        }
+        if (msgl == "super armour" || msgl == "super armor"){
+            message.channel.sendMessage("**Super Armor:**\nCan only withstand 1 attack without being interrupted (I.E Shugoki has passive super armor and loses it on being hit).");
+        }
+        if (msgl == "dazed" || msgl == "daze"){
+            message.channel.sendMessage("**Dazed:**\nAttacks and abilities that cause the dazed status cause your screen to flash white and they temporarily lose your HUD which makes it more difficult to block a follow-up attack.");
+        }
+        if (msgl == "wall stun"){
+            message.channel.sendMessage("**Wall Stun:**\nThis effect requires you to throw an opponent into a wall using the GuardBreak throw ability (or other specific abilities). The Wall Stun temporarily staggers your opponent against the wall for a long period of time. Usually enough for someone to capitalize with a powerful attack. ");
+        }
+        if (msgl == "superior"){
+            message.channel.sendMessage("**Superior Block:**\nYour character's block stops all attacks from being able to chain into other attacks. This will not block unblockable attacks.");
+        }
+        if (msgl == "chip damage" || msgl == "block damage" || msgl == "chip damage/block damage"){
+            message.channel.sendMessage("**Chip Damage/block damage:**\nDamage you take when you block a heavy attack.");
+        }
+        if (msgl == "comamnd grab"){
+            message.channel.sendMessage("**Command Grab:**\nA command a player can input with certain characters that moves/damages another player without them being able to defend against or react if hit. These moves are usually unblockable.");
+        }
+        if (msgl == "poke"){
+            message.channel.sendMessage("**Poke:** A generally quick attack that's used at your character's maximum range too; Create distance between two players; Stuff an opponent's current attack (Or counter poke); or deal \"Safe\" damage");
+        }
+        if (msgl == "safe"){
+            message.channel.sendMessage("**Safe:** Attacks that are considered safe usually don't have long recovery times, meaning you can take actions very soon after the animation completes. Attacks that are \"Unsafe\" usually have long recovery times and leave you very open to attacks.");
+        }
+        if (msgl == "punish"){
+            message.channel.sendMessage("**Punish:**\nAttacking an opponent who is in a recovery animation from performing an attack or whiffing an attack.");
+        }
+        if (msgl == "priority" || msgl == "priority low" || msgl == "priority medium" || msgl == "piority high" || msgl == "priority(high/medium/low)"){
+            message.channel.sendMessage("**Priority(High/Medium/Low):**\nAttacks that can outright beat other attacks or actions being performed by another character. Example: A character who's in the middle of a light attack will always take priority over a character trying to guard break.");
+        }
+        if (msgl == "read" || msgl == "reading"){
+            message.channel.sendMessage("**Read/Reading:**\nReading is when you understand your opponent's playstyle and patterns so you're better able to guess their next move, and counter it.");
+        }
+        if (msgl == "recovery"){
+            message.channel.sendMessage("**Recovery:**\nThe time it takes for you to recover from the animation so you can take another.");
+        }
+        if (msgl == "neutral"){
+            message.channel.sendMessage("**Neutral:**\nThe part of a fight in which no one fighter is taking definitive actions against the other. It's generally referred to as \"feeling out\" the fight.");
+        }
+        if (msgl == "neutral stance"){
+            message.channel.sendMessage("**Neutral stance:**\nThis is unique to assassin heroes and is the stance they ");
+        }
+        if (msgl == "whiff"){
+            message.channel.sendMessage("**Whiff:**\nA move that misses completely. Can be intentional to bait an opponent into a chain that doesn't require a hit.");
+        }
+        if (msgl == "zoning"){
+            message.channel.sendMessage("**Zoning:**\nA tactic, usually used by characters with weapons with more range than their opponents to keep them at bay, and also to take higher priority with their attacks. Example: A Kensei keeping distance from a Warden by Poking, and using his range to his advantage.");
+        }
+        if (msgl == "tracking"){
+            message.channel.sendMessage("**Tracking:** Attacks that \"follow\" the opponents movement. Attacks with tracking must be dodged at the correct time otherwise, they will still hit you.\nExample: Orochi's Hurricane Blast will track and move along with opponents, making it harder to side-step, or back away from.");
+        }
+        if (msgl == "weapon effects"){
+            message.channel.sendMessage("**Weapon effects:**\nThese are colors used to indicate what each attack does or can do.\n\n**Blue** means the attack can be chained into another attack\n**Yellow** means the attack is a counter attack or has the block property (I.E Crushing counter or Warlord counter light attacks)\n**Orange and fiery** means the attack or ability is unblockable.\n**Purple** means that the attack will apply the bleed effect.");
+        }
         if (["peacekeeper", "peacekeepers", "class peacekeeper", "class peacekeepers"].includes(msgl)) {
             message.channel.sendMessage("__**Peacekeeper**__\n**Difficulty**: Medium\n**Fighting Style** Counter Attacker\nShort Range, High Mobility\nVery fast attacks. Use dagger to cancel or confirm attacks into bleed damage and a deflect ability");
         }
