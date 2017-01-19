19cb021fcd595b37635f5430466d49f694c945ce
jjplay175 <jjplay175@hotmail.com>
2017-01-19 03:35:55 +0000

    V 1.1.2 Added - Population checks

--- bot.js
+++ bot.js
@@ -127,6 +127,21 @@ UnityBot.on("message", function (message) {
         return;
     }
 
+    if (cmd == "discord") {
+        if (msgl == "platform" || msgl == "playforms") {
+            pcQ = message.guild.roles.get("269324174344847361").members.size;
+            xboxQ = message.guild.roles.get("269324137778905088").members.size;
+            ps4Q = message.guild.roles.get("269324186042892289").members.size;
+            message.channel.sendMessage(`Who have we got in here?\n**PC:** ${pcQ}\n**Xbox:** ${xboxQ}\n**Ps4:** ${ps4Q}`);
+        }
+        if (msgl == "faction" || msgl == "factions") {
+            knightsQ = message.guild.roles.get("256619278273478656").members.size;
+            vikingsQ = message.guild.roles.get("256619528945926145").members.size;
+            samuraiQ = message.guild.roles.get("256618627787128832").members.size;
+            message.channel.sendMessage(`Scouts show the faction populations are\n**Knights:** ${knightsQ}\n**Vikings:** ${vikingsQ}\n**Samurai** ${SamuraiQ}`);
+        }
+    }
+
     //French Help
     if (cmd === "aide") {
         if (message.channel.id == "254691230569463809") {
