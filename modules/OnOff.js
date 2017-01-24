6ddda540512f0dcda089b097026c18e254c1e1dd
jjplay175 <jjplay175@hotmail.com>
2017-01-24 16:07:10 +0000

    Same update.

--- modules/OnOff.js
+++ modules/OnOff.js
@@ -6,7 +6,7 @@ function On(ComData,message) {
     if (ComData.Bot == "enabled") return message.channel.sendMessage("The bot is already fully functional?");
     ComData.Bot = "enabled";
     message.channel.sendMessage("**Fuctionality Restored**")
-    fs.writeFile("..configure/commands.json", JSON.stringify(ComData), function (err) {
+    fs.writeFile("../configure/commands.json", JSON.stringify(ComData), function (err) {
         if (err) {
             return console.log(err);
         }
@@ -17,7 +17,7 @@ function On(ComData,message) {
 function Off(ComData,message) {
     ComData.Bot = "disabled";
     message.reply("I will no longer accept any commands other than \"!activate\" and will no longer reply to any requests.\n**Disabled**")
-    fs.writeFile("..configure/commands.json", JSON.stringify(ComData), function (err) {
+    fs.writeFile("../configure/commands.json", JSON.stringify(ComData), function (err) {
         if (err) {
             return console.log(err);
         }
